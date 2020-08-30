c71355bd5418859d3b5c09cd619e64ee2c4ed03f
luisandres248 <luisandres248@gmail.com>
2020-08-30 03:30:03 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn.ensemble import IsolationForest
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -14,3 +14,26 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+
+class OutlierExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        print('init')
+
+    def fit(self, X, Y):
+        return self
+
+    def transform(self, X, Y):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        X2 = si.fit_transform(X)
+        Y2 = Y.to_numpy()
+        iso = IsolationForest(n_estimators=400,contamination=0.3,random_state=42)
+        yhat = iso.fit_predict(X2)
+        mask = yhat != -1
+        X2, Y2 = X2[mask, :], Y2[mask]
+        print(X2.shape)
+        print(Y2.shape)
+        X_train2 = pd.DataFrame.from_records(data=X2,columns=X.columns)
+        Y_train2 = Y2
+
+        return (X_train2, Y_train2)
